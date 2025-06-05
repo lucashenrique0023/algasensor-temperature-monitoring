fc8875a4d313e76b49144a8d9fe8aea210cb0c2f
Lucas Henrique <lucas.henrique0023@gmail.com>
2025-06-05 16:11:57 -0300

    New project files

---
+++ src/test/java/lab/lhss/algasensor/temperature/monitoring/TemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.lhss.algasensor.temperature.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
